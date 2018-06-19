7f24fd6e314215b533039a143c8930d758f6cae9
shangxzn <1760783555@qq.com>
2018-06-20 00:14:57 +0800

    再次提交一下

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -16,10 +16,43 @@ $(function () {
       },
       success: function (info) {
         console.log(info);
-        $('tbody').html( template('tpl', info) );
+        $('tbody').html(template('tpl', info));
+
+        // 初始化一个分页
+        $("#paginator").bootstrapPaginator({
+          bootstrapMajorVersion: 3,
+          currentPage: page,
+          totalPages: Math.ceil(info.total / info.size),
+          size: 'small',
+          onPageClicked: function (a, b, c, p) {
+            page = p;
+            render();
+          }
+        });
       }
     });
   }
+
+
+  //添加二级分类
+  $(".btn_add").on("click", function () {
+    $("#addModal").modal("show");
+
+    //发送ajax请求，获取所有的一级分类的数据
+    $.ajax({
+      type: 'get',
+      url: '/category/queryTopCategoryPaging',
+      data: {
+        page: 1,
+        pageSize: 100
+      },
+      success: function (info) {
+        console.log(info);
+        $(".dropdown-menu").html(template("tpl2", info));
+      }
+    });
+  });
+
 
 
 
