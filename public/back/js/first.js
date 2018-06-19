d912fab8ac0e233b42ec8c4a934f564a1c2933bf
shangxzn <1760783555@qq.com>
2018-06-19 20:37:27 +0800

    解决了这次出现报错的一些问题，因为函数调用在变量前面了

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -1,12 +1,12 @@
 
 
 $(function () {
-
-  render();
+ 
+ 
 
   var page = 1;
   var pageSize = 8;
-
+  render();// 这个不能放在最上面，因为预解析的时候，page和pageSize并没有赋值就运行了函数，那data中page=page就没办法执行
   function render() {
 
     $.ajax({
