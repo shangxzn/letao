fd7e6086bd47082ccb186adbf20358d005cbecc2
shangxzn <1760783555@qq.com>
2018-06-21 16:27:30 +0800

    首页功能完成

--- public/front/js/common.js
+++ public/front/js/common.js
@@ -1,9 +1,11 @@
-//初始化mui的区域滚动
+// //初始化mui的区域滚动
 mui('.mui-scroll-wrapper').scroll({
-  indicators: false, //不显示滚动条
+	indicators: false,//不显示滚动条
 });
 
-//初始化轮播图
+// //初始化轮播图
+
+//获得slider插件对象
 mui('.mui-slider').slider({
   interval:5000//自动轮播周期，若为0则不自动播放，默认为0；
 });
